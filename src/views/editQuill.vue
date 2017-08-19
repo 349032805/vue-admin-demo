2ea13d820d3e8154ed5caeaed4a287d83658b91e
349032805 <349032805@qq.com>
2017-08-19 22:30:03 +0800

    git commit

--- src/views/editQuill.vue
+++ src/views/editQuill.vue
@@ -1,25 +1,47 @@
 <template>
   <p-layout>
-    <p>test</p>
+    <div class="plugins-tips">
+      Vue-Quill-Editor：基于Quill、适用于Vue2的富文本编辑器。 访问地址：
+      <a href="https://github.com/surmon-china/vue-quill-editor" target="_blank">vue-quill-editor</a>
+    </div>
+    <quill-editor ref="myTextEditor" v-model="content" :config="editorOption"></quill-editor>
+    <el-button class="editor-btn" type="primary" @click="submit">提交</el-button>
   </p-layout>
 </template>
 
-<script type="text/ecmascript-6">
-  export default {
-    data() {
-      return {
-
+<script>
+import { quillEditor } from 'vue-quill-editor';
+export default {
+  data: function () {
+    return {
+      content: '<p>Hi girl</p>',
+      editorOption: {
+        // something config
       }
+    }
+  },
+  components: {
+    quillEditor
+  },
+  methods: {
+    onEditorChange({ editor, html, text }) {
+      this.content = html;
     },
-    created() {
-      console.log("---------create");
-    },
-    methods: {
-        
-    }  
-  };
+    submit() {
+      console.log(this.content);
+      this.$message.success('提交成功！');
+    }
+  },
+  computed: {
+    editor() {
+      return this.$refs.myTextEditor.quillEditor;
+    }
+  }
+}
 </script>
-
 <style lang="less" scoped>
-	
+  .plugins-tips{
+    padding:20px 10px;
+    margin-bottom: 20px;
+  }
 </style>
